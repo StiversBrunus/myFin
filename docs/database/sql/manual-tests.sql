cfd8029c5e65db916fbd685be810116d7f0444f3
StiversBrunus <bruno.oliveira_araujo@outlook.com>
2026-07-13 18:38:47 -0300

    docs: adicionando drop table para limpar o banco de testes.

--- docs/database/sql/manual-tests.sql
+++ docs/database/sql/manual-tests.sql
@@ -996,7 +996,11 @@ DELETE FROM goal;
 DELETE FROM reserve;
 DELETE FROM wallet;
 
+DROP TABLE wallet, reserve, goal, bank, investment, investment_type, investment_transaction;
 
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public' AND table_type = 'BASE TABLE';
 
 
 
